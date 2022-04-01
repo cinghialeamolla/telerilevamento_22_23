8d648cf4c4f25beb01765a4ae1f6ff2cbc5e0808
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-04-01 17:06:02 +0200

    Update R_code_time_series_greenland.r

--- R_code_time_series_greenland.r
+++ R_code_time_series_greenland.r
@@ -7,4 +7,18 @@ setwd("C:/lab/greenland") # Windows #settare la cartella greenland
 # setwd("/Users/name/Desktop/lab/greenland") # Mac
 
 #funzione per creare un singolo layer (strato)
-raster("")
+lst2000 <- raster("lst_2000.tif")
+lst2000
+
+plot(lst2000)
+
+# importare i dati anche per il 2005, 2010 e 2015
+lst2005 <- raster("lst_2005.tif")
+lst2010 <- raster("lst_2010.tif")
+lst2015 <- raster("lst_2015.tif")
+
+#multiframe di 4 dati greenland
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+
+
+
