47f36387306f410269d00562c4914c19e66bd27c
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-04-01 16:57:00 +0200

    Create R_code_time_series_greenland.r

---
+++ R_code_time_series_greenland.r
@@ -0,0 +1,10 @@
+# Time series analysis of Greenland LST data
+
+library(raster)
+
+# setwd("~/lab/greenland") # Linux
+setwd("C:/lab/greenland") # Windows #settare la cartella greenland
+# setwd("/Users/name/Desktop/lab/greenland") # Mac
+
+#funzione per creare un singolo layer (strato)
+raster("")
