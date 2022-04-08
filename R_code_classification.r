c66de1afc8cdfd9e5d90905ef041821983365e49
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-04-08 17:27:18 +0200

    Create R_code_classification.r

---
+++ R_code_classification.r
@@ -0,0 +1,8 @@
+# Classificazione automatica (fatta dal softwere) dei pixel
+
+library(raster)
+library(RStoolbox)
+
+# setwd("~/lab/") # Linux
+setwd("C:/lab/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac 
