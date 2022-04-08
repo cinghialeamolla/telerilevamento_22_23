12dc9a8d661dac6ed1ce65e5888bcf879b6f5621
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-04-08 17:50:19 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,8 +1,25 @@
 # Classificazione automatica (fatta dal softwere) dei pixel
 
+#richiamare le librerie
 library(raster)
 library(RStoolbox)
 
+#settare la cartella di lavoro
 # setwd("~/lab/") # Linux
 setwd("C:/lab/") # Windows
 # setwd("/Users/name/Desktop/lab/") # Mac 
+
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg") #importa il file e lo assegna al nome
+so
+
+plotRGB(so, r=1, g=2, b=3, stretch="lin") #plot in RGB del file
+
+#classificazione dei dati
+soc <- unsuperClass(so, nClasses=3) #suddivisione in 3 classi dei pixel del file (la classificazione è fatta automaticamente dal softwere)
+soc  #la funzione sopra crea un modello con unico output una mappa: classificazione e creazione di output che contiene una mappa
+
+cl <- colorRampPalette(c('yellow','black','red'))(100) #palette con 3 classe
+plot(soc$map, col=cl) #plot della mappa appena creata con 3 classi
+
+
+
