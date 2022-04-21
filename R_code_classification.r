98d54e5a93094b96012e10976b965857675081f9
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-04-21 17:57:23 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -23,3 +23,34 @@ plot(soc$map, col=cl) #plot della mappa appena creata con 3 classi
 
 # set.seed può essere usato per ripetere un esperimento
 
+#### DAY 2 Grandcanyon ####
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg") #per importare il file su R
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+
+gcclass2 <- unsuperClass(gc, nClasses=2) #per classificare in 2 classi i pixel dell'immagine
+gcclass2
+plot(gcclass2$map) #plot delle 2 classi
+
+# Esercizio, classificare la mappa in 4 classi
+gcclass4 <- unsuperClass(gc, nClasses=4) 
+gcclass4
+
+clc <- colorRampPalette(c('yellow','red','blue','black'))(100)
+par(mfrow=c(2,1))
+plot(gcclass2$map, col=clc)
+plot(gcclass4$map, col=clc)
+
+# comparare la mappa classificata con quella originale
+par(mfrow=c(2,1))
+plot(gcclass4$map, col=clc)
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+#fare lo "stack" per migliorare la restituzione
+st <- stack(gc, gcclass4$map)
+plot(st) 
+
+
+
+
+
