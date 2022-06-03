b62aff5dcf4cf8b14a0cf8cfc6820d9e9b7843a0
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-06-03 16:38:03 +0200

    Update R_code_create_new_function.r

--- R_code_create_new_function.r
+++ R_code_create_new_function.r
@@ -22,3 +22,23 @@ cheer_me_n_times <- function(your_name, n) {
 cheer_me_n_times(n=3, your_name="Pamela")
 cheer_me_n_times("Pamela", 3)
 
+#settare la cartella di lavoro
+# setwd("~/lab/") # Linux
+setwd("C:/lab/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac
+library(raster)
+dato <- raster("sentinel.png")
+plot(dato)
+
+plot_raster <- function(r, col=
+                        NA) {
+    if(!is.na(col)) {
+        pal <- colorRampPalette(col)(100)
+        plot(r, col=pal)
+        } else {
+        plot(r)
+        }
+    }
+plot_raster(dato, c("brown", "yellow", "green"))
+    
+
