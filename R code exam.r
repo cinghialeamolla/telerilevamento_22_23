72ce04961e6e4c7dde82287437243d9d3d9a1000
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2023-02-09 00:04:00 +0100

    Create R code exam.r

---
+++ R code exam.r
@@ -0,0 +1,8 @@
+#Qui sotto il codice oggetto di esame
+
+#Per prima cosa, richiamiamo il pacchetti che serviranno per le analisi
+library(raster)
+
+#poi faccio il settaggio della cartella di lavoro 
+setwd("C:/rexam/")  #questa formula si usa per windows
+
