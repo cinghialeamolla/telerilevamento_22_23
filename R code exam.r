3d3c58ab7aed0ec5a557dffb9bfd0e3648b29357
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2023-02-12 13:29:47 +0100

    Update R code exam.r

--- R code exam.r
+++ R code exam.r
@@ -1,8 +1,20 @@
 #Qui sotto il codice oggetto di esame
 
-#Per prima cosa, richiamiamo il pacchetti che serviranno per le analisi
+# dati.nc: il formato dei dati copernicus è nc, e per analizzarli occorre istallare alcuni pacchetti su R
+
+# ncdf4 è un pacchetto che fornisce file di dati scritti utilizzando la libreria netCDF di Unidata.
+# la funzione sottostante si usa per istallare dei pacchetti esterni, dal CRAN with the use of " "
+install.packages("ncdf4")
+
+# sp è un pacchetto per i dati spaziali
+install.packages("sp")
+
+# Queste funzioni richiamano i pacchetti usati per le analisi
+library(ncdf4)
 library(raster)
 
-#poi faccio il settaggio della cartella di lavoro 
+library(raster)
+
+# "setwd" si usa per il settaggio della cartella di lavoro 
 setwd("C:/rexam/")  #questa formula si usa per windows
 
